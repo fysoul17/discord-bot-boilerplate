d4bd093a514b149013cbc6e07706e2046c1348ed
fysoul17 <terry@gixgames.com>
2022-11-21 00:56:09 +0900

    add REST setting for public bot

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -5,7 +5,11 @@ export const onReady = async (BOT: Client) => {
   const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN as string);
   const commandData = CommandList.map((command) => command.data.toJSON());
 
-  await rest.put(Routes.applicationGuildCommands(BOT.user?.id || "missing id", process.env.GUILD_ID as string), { body: commandData });
+  // Public Bot
+  await rest.put(Routes.applicationCommands(BOT.user?.id || "missing id"), { body: commandData });
+
+  // Private Bot
+  //await rest.put(Routes.applicationGuildCommands(BOT.user?.id || "missing id", process.env.GUILD_ID as string), { body: commandData });
 
   console.log("Bot is ready!");
 };
