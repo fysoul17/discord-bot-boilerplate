61afcc9aa0b72617a12c68926d190553efcb3324
fysoul17 <terry@gixgames.com>
2022-12-08 16:42:59 +0900

    add mongo db name setting

--- src/database/DBConnection.ts
+++ src/database/DBConnection.ts
@@ -3,12 +3,15 @@ import mongoose from "mongoose";
 export const dbConnection = async () => {
   const mongoCluster: string = process.env.MONGO_CLUSTER as string;
 
-  let mongoUser: string = process.env.MONGO_USER as string;
-  let mongoPassword: string = process.env.MONGO_PASSWORD as string;
+  const mongoUser: string = process.env.MONGO_USER as string;
+  const mongoPassword: string = process.env.MONGO_PASSWORD as string;
 
   const mongoUri = mongoCluster.replace("<username>", mongoUser).replace("<password>", mongoPassword);
 
-  await mongoose.connect(mongoUri);
+  const mongoDbName: string = process.env.MONGO_DB_NAME as string;
+  await mongoose.connect(mongoUri, { dbName: mongoDbName });
 
   console.log("Database Connected!");
+  console.log("Uri: " + mongoUri);
+  console.log("DB Name: " + mongoDbName);
 };
